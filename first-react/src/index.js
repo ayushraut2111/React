d45f2373100ebd4d230110474f7ea8021276ac56
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-04-26 18:04:18 +0530

    map function

--- first-react/src/index.js
+++ first-react/src/index.js
@@ -3,47 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
-const book2={
-  img:"https://images-eu.ssl-images-amazon.com/images/I/41-Jq9RDQwL._AC_SX184_.jpg",
-  title:"ayush",
-  number:2
-}
+// const book2={
+//   img:"https://images-eu.ssl-images-amazon.com/images/I/41-Jq9RDQwL._AC_SX184_.jpg",
+//   title:"ayush",
+//   number:2
+// }
 
-const Booklist=()=>{
-  return(
-    <div>
-      <Book 
-      img="https://m.media-amazon.com/images/I/91MDUJ1ejuS._AC_UL480_FMwebp_QL65_.jpg"
-      author="ayush"
-      title="hello i am a prop"
-      >   
-      <div style={{border:'2px solid red',display:'inline-block'}}>
-        <h5>
-          Children prop
-        </h5>
-        <p>Hello i am children</p>
-      </div>
-      </Book>
-      <Book
-      img={book2.img}
-      title={book2.title}
-      // number=2 we cant assign number like this we have to use curly braces to identify that we are using javascript
-      number={book2.number} 
-      />   
-    </div>
-  );
-}
-const Book=({number,img,author,title,children})=>{
-  return (
-    <article>
-    <img src={img} alt="" />
-    <h1>{author}</h1>
-    {children}
-    <p>{title}</p>
-    <p>{number}</p>
-    </article>
-  );
-}
+// const Booklist=()=>{
+//   return(
+//     <div>
+//       <Book 
+//       img="https://m.media-amazon.com/images/I/91MDUJ1ejuS._AC_UL480_FMwebp_QL65_.jpg"
+//       author="ayush"
+//       title="hello i am a prop"
+//       >   
+//       <div style={{border:'2px solid red',display:'inline-block'}}>
+//         <h5>
+//           Children prop
+//         </h5>
+//         <p>Hello i am children</p>
+//       </div>
+//       </Book>
+//       <Book
+//       img={book2.img}
+//       title={book2.title}
+//       // number=2 we cant assign number like this we have to use curly braces to identify that we are using javascript
+//       number={book2.number} 
+//       />   
+//     </div>
+//   );
+// }
+// const Book=({number,img,author,title,children})=>{
+//   return (
+//     <article>
+//     <img src={img} alt="" />
+//     <h1>{author}</h1>
+//     {children}
+//     <p>{title}</p>
+//     <p>{number}</p>
+//     </article>
+//   );
+// }
 // const Book=(props)=>{
 //   const {number,img,author,title}=props;
 //   return (
@@ -56,10 +56,16 @@ const Book=({number,img,author,title,children})=>{
 //   );
 // }
 
+const array=['ayush','chaurasia','anish'];
+array.map((book)=>
+{
+  console.log(book);
+});
+
 // ReactDOM.render(<Booklist/>,document.getElementById('root'));
-const container=document.getElementById('root');
-const root=ReactDOM.createRoot(container);
-root.render(<Booklist/>)
+// const container=document.getElementById('root');
+// const root=ReactDOM.createRoot(container);
+// root.render(<Booklist/>)
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
