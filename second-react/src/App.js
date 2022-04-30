476f8be86d8c64c5aa2385f62ce586f46b1aed28
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-04-30 16:16:02 +0530

    testing grid with components

--- second-react/src/App.js
+++ second-react/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import Objects from './objects'
 
 const App=()=>
 {
@@ -8,7 +8,27 @@ const App=()=>
     <h1 className="mainheading">
       hello i am heading
     </h1>
+    <div className="content">
+      {
+        Objects.map((obj)=>
+        {
+          return (<Body {...obj}/>)
+        }
+        )
+      }
+    </div>
   </div>
+  );
+}
+const Body=(props)=>
+{
+  const {id,name,lastname}=props;
+  return(
+    <div className="body">
+      <h4>{id}</h4>
+      <h1>{name}</h1>
+      <h2>{lastname}</h2>
+    </div>
   );
 }
 
