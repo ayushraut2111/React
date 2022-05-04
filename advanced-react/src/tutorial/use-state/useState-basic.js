3032e0ccafb065e9eac16406bcf351d29693484e
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-05 00:04:28 +0530

    usestate understand

--- advanced-react/src/tutorial/use-state/useState-basic.js
+++ advanced-react/src/tutorial/use-state/useState-basic.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 // useState is a inbuilt function
 const USeStateBaiscs=()=>
-{
-    console.log(useState())
+{ 
+    console.log(useState('hello world'))    // its output is [hello world, f] that means it is returning array so we can access it like array
     return(
         <React.Fragment>
             <h2>UseStateBasic example</h2>
