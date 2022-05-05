f4f3238aedbe1829cf2cc152aabed0d2e95e54ab
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-05 23:40:03 +0530

    filter function

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 import Setup from './tutorial/use-state/useState-array';
+let a=[1,2,3,4];
+
+let b=a.filter((content)=>{
+  return(content!=2&&content!=3);              //filter function filters the array according to the given condition
+})
+console.log(b);
 function App() {
   return (
     <div className="App">
