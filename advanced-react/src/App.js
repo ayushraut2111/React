73c636422543ec8650717eb9c8ef35a42fd8181c
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-05 23:57:47 +0530

    usestate object

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,11 +1,5 @@
 import './App.css';
-import Setup from './tutorial/use-state/useState-array';
-let a=[1,2,3,4];
-
-let b=a.filter((content)=>{
-  return(content!=2&&content!=3);              //filter function filters the array according to the given condition
-})
-console.log(b);
+import Setup from './tutorial/use-state/useState-object';
 function App() {
   return (
     <div className="App">
---
+++ advanced-react/src/tutorial/use-state/useState-object.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const UseStateObject=()=>{
+    const [person,setPerson]=React.useState({name:"ayush",age:20,message:"hello world"});
+    const Chg=()=>{
+        setPerson({...person,message:"I am ayush"})
+    }
+    return(
+        <>
+        <h3>{person.name}</h3>
+        <h3>{person.age}</h3>
+        <h3>{person.message}</h3>
+        <button onClick={Chg}>Change</button>
+        </>
+    );
+}
+
+export default UseStateObject;
