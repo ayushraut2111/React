38e135ca85724f993101e7f2c08290ad78add73f
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-06 00:01:01 +0530

    object spread operator in js

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 import Setup from './tutorial/use-state/useState-object';
+
+const a={name:"ayush",class:3,message:"hello"};
+console.log(a);
+const b={...a,name:"anish"};    // this is a spread operator in object this will first copy everyting in object b and then change only name to anish
+console.log(b);
+
 function App() {
   return (
     <div className="App">
