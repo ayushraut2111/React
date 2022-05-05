27e9821913ebd0578672f08242896f90682f188e
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-05 22:45:06 +0530

    setting data for usestate array

---
+++ advanced-react/src/data.js
@@ -0,0 +1,4 @@
+export const data=[
+    {id:1,name:"ayush"},
+    {id:2,name:"anish"}
+];
--- advanced-react/src/tutorial/use-state/useState-array.js
+++ advanced-react/src/tutorial/use-state/useState-array.js
@@ -1,7 +1,10 @@
 import React,{useState} from 'react';
+import {data} from '../../../src/data'   // .. means going to the back folder
+// instead of importing {useState} we can also use React.useState wherever we want to use useState
 
 const UseStateArray=()=>
 {
+    console.table(data);
     return(
         <React.Fragment>
             <h3>Hello i am usestate array</h3>
