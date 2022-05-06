4c3a8cff789a168cc54fd61bb9a558a79d99a9da
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-07 00:24:15 +0530

    useeffect basics example

--- advanced-react/src/tutorial/use-effect/useEffect-basic.js
+++ advanced-react/src/tutorial/use-effect/useEffect-basic.js
@@ -2,10 +2,17 @@ import React, {useState,useEffect} from "react";
 
 const UseEffectBasic=()=>
 {
+    const [value,setValue]=useState(0);
+    useEffect(()=>{
+        document.title=`my value is ${value}`
+        console.log("i am rendered");
+    });
     return(
-        
         <>
         <h1>UseEffectBasics</h1>
+        <hr/>
+        <h1>{value}</h1>
+        <button className="btn" onClick={()=>{setValue(value+1)}}>Click me</button>
         </>
     );
 }
