1bbc5102c169ffd9ecacf2587358122cbaf12f56
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-07 01:31:49 +0530

    conditional in use effect

--- advanced-react/src/tutorial/use-effect/useEffect-basic.js
+++ advanced-react/src/tutorial/use-effect/useEffect-basic.js
@@ -3,9 +3,9 @@ import React, {useState,useEffect} from "react";
 const UseEffectBasic=()=>
 {
     const [value,setValue]=useState(0);
-    useEffect(()=>{
+    useEffect(()=>{                       
         console.log("i am rendered");
-        if(value%2==0)
+        if(value%2==0)                      // we can not use useEffect inside a conditional statement but we can use conditional statement in useeffect and usestate
         document.title=`my value is ${value}`;
         else
         document.title='title';
