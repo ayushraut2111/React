4c0f69f7bdd31ae3e505c402577e75396825f076
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-07 22:31:08 +0530

    display size change with useeffect

--- advanced-react/src/tutorial/use-effect/useEffect-cleanup.js
+++ advanced-react/src/tutorial/use-effect/useEffect-cleanup.js
@@ -1,11 +1,25 @@
 import React,{useState,useEffect} from "react";
-
+import '../../App.css'
 const UseEffectCleanup=()=>{
+    const [size1,setSize1]=useState(window.innerHeight);
+    const [size2,setSize2]=useState(window.innerWidth);
+
+    const sizechange=()=>{
+        setSize1(window.innerHeight);
+        setSize2(window.innerWidth);
+    }
+
+    useEffect(()=>{
+        window.addEventListener('resize',sizechange);
+    });
+
 return(
     <>
     <h1>
-        hello
+        Window size 
     </h1>
+    <h2 className="size" >height is {size1}</h2>
+    <h2 className="size" >widht is {size2}</h2>
     </>
 );
 }
