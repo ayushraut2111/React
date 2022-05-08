921bf50a43d8112f91b0725df7551c11e0488f06
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-08 10:48:09 +0530

    useeffect infinite loop

--- advanced-react/src/tutorial/use-effect/useEffect-fetchdata.js
+++ advanced-react/src/tutorial/use-effect/useEffect-fetchdata.js
@@ -4,12 +4,36 @@ import '../../App.css';
 const url='https://api.github.com/users';
 const UseEffectFetchData=()=>
 {
+    const [user,setUser]=useState([]);
+
+    const GetUsers=async()=>{
+        const response= await fetch(url);
+        const users=await response.json();        
+        setUser(users);               // here we are using setusers inside a useeffect so everytime setusers triggers component 
+        // re renders and useeffect called again and inside use effect setusers called gain so it gets into infinite loop
+    };
+
+    useEffect(()=>{
+        GetUsers();
+    },[]);    // so to remove the infinite loop we only renders it once by giving it second argument []
     return(
         <>
         <h1>
             Fetching data
+            <div className="main">
+                {
+                    user.map((people)=>{
+                        return <Uselist key={people.id}{...people}/>
+                    })
+                }
+            </div>
         </h1>
         </>
     );
 }
+const Uselist=(props)=>{
+    console.log(props);
+    const {id,login,avatar_url,html_url}=props;
+
+};
 export default UseEffectFetchData;
