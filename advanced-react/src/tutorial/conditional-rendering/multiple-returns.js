1b4d29b724b04f5aba3fc0cc8f048529b8c20aeb
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-08 22:13:28 +0530

    multiple return

--- advanced-react/src/tutorial/conditional-rendering/multiple-returns.js
+++ advanced-react/src/tutorial/conditional-rendering/multiple-returns.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 
 const MultipleReturn=()=>{
-    return(
-        <div>
-            <h1>
-                hello
-            </h1>
-        </div>
-    );
+    const [loading,setLoading]=useState(true);
+    if(loading)
+    {
+        
+        return <h1>loading....</h1>
+    }
+    else
+    {
+        return <h1>not loading.....</h1>
+    }
 };
 
 export default MultipleReturn;
