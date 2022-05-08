18dc3a027748cc8d714edcc41f8b114b1230a762
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-08 23:19:36 +0530

    fetch data with conditional return statement

--- advanced-react/src/tutorial/conditional-rendering/multiple-returns.js
+++ advanced-react/src/tutorial/conditional-rendering/multiple-returns.js
@@ -6,7 +6,7 @@ const MultipleReturn=()=>{
 
     useEffect(()=>{
         fetch(url)    // at first when it renders first time it starts returning loading and then use effect calls and then as soon as it fetch data it sets the loading as false and then it fall into else condition of the return statement
-        .then((resp)=>resp.json())     // to watch the effect slow down the network speed from console
+        .then((resp)=>resp.json())     // to watch the effect slow down the network speed from console.
         .then((res)=>{
             const{login}=res;
             setLoading(false);
