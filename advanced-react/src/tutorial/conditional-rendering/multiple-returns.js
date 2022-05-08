5c955dd0866b97208ab56deeee69e6e81fa5a630
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-08 23:19:05 +0530

    multiple return with use effect and use state

--- advanced-react/src/tutorial/conditional-rendering/multiple-returns.js
+++ advanced-react/src/tutorial/conditional-rendering/multiple-returns.js
@@ -1,15 +1,27 @@
 import React, {useState,useEffect} from "react";
-
+const url='https://api.github.com/users/ayushraut2111';
 const MultipleReturn=()=>{
     const [loading,setLoading]=useState(true);
+    const [user,setUser]=useState('default user')
+
+    useEffect(()=>{
+        fetch(url)    // at first when it renders first time it starts returning loading and then use effect calls and then as soon as it fetch data it sets the loading as false and then it fall into else condition of the return statement
+        .then((resp)=>resp.json())     // to watch the effect slow down the network speed from console
+        .then((res)=>{
+            const{login}=res;
+            setLoading(false);
+            console.log("ayush")
+            setUser(login);
+        })
+    },[]);
+
     if(loading)
-    {
-        
+    { 
         return <h1>loading....</h1>
     }
     else
     {
-        return <h1>not loading.....</h1>
+        return <h1>{user}</h1>
     }
 };
 
