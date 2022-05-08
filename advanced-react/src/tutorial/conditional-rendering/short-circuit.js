7b7c9bf9f30ad97cbd95ad853177100aad552338
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-09 00:18:13 +0530

    short circuiting

--- advanced-react/src/tutorial/conditional-rendering/short-circuit.js
+++ advanced-react/src/tutorial/conditional-rendering/short-circuit.js
@@ -1,10 +1,14 @@
 import React,{useState} from "react";
 
 const ShortCircuit=()=>{
+    var text='';
     return(
-        <h1>
-            hello
-        </h1>
+        <>
+        <h1>{text||'ayush'}</h1>      {/* if text is false then return second statement i.e. ayush */}
+        <h1>{!text&&"hello"}</h1>     {/* if text is true only then execute the second statement */}
+        {!text&&  <div><h1>hello</h1><h2>ayush</h2></div>}    {/* we can use short circuit in both way this is same as above */} 
+        {/* if we want to execute more statements in conditional then we can use this in {} like above */}
+        </>
     );
 };
 
