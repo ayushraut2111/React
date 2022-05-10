cb0908a5a5ed3e5514837c211e7cac08dfb8f408
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-11 00:03:13 +0530

    ternary operator

--- advanced-react/src/tutorial/conditional-rendering/ternary-operator.js
+++ advanced-react/src/tutorial/conditional-rendering/ternary-operator.js
@@ -1,12 +1,15 @@
 import React,{useState} from "react";
-
+import '../../App.css'
 const TernaryOperator=()=>{
     const[text,setText]=useState('');
     const [isError,setIsError]=useState(false);
     return(
         <>
         <h1>{text||'ayush'}</h1>     
-        
+        <button type="button" className="btn" onClick={()=>setIsError(!isError)}>Toggle Error</button>
+        {isError? <h1>There is a Error....</h1>:<h1>no error....</h1>}  
+        {/* unlike and /or operator ternary operaor return two value one for false and other for true */}
+ 
         </>
     );
 };
