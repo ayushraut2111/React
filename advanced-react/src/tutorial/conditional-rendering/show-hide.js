56143b4a7aaae979e85a769fd891fc9de7fe0939
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-13 22:45:10 +0530

    show and hide another component

--- advanced-react/src/tutorial/conditional-rendering/show-hide.js
+++ advanced-react/src/tutorial/conditional-rendering/show-hide.js
@@ -7,7 +7,7 @@ const Showhide=()=>{
     return(
         <>
         <button className="btn" onClick={()=>{setShow(!show)}}>show/hide</button>
-        {show&&<Item/>}
+        {show&&<Item/>}   {/* if the show is true then it will output the component item*/}
         </>
     );
 }
@@ -19,12 +19,12 @@ const Item=()=>{
     }
 
     useEffect(()=>{
-        window.addEventListener('resize',Changesize);
+        window.addEventListener('resize',Changesize);    
 
         return ()=> {
-            window.removeEventListener('resize',Changesize);
+            window.removeEventListener('resize',Changesize);  // here we have to use cleanup function because when exiting use effect it will remove the eventlistener when componenet is not showing
         }
-    });
+    });   // here we will not use [] because this will run everytime we show the component and will remain even after exiting useeffect so we have to clear the eventlistener
 
     return(
         <>
