8f2ea3ec434ed66a6fa478e8cc439553914c9c1b
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-14 22:35:20 +0530

    saving inouts of a form in react

--- advanced-react/src/tutorial/forms/controlled-inputs.js
+++ advanced-react/src/tutorial/forms/controlled-inputs.js
@@ -1,25 +1,28 @@
-import React from "react";
+import React ,{useState} from "react";
 import '../../App.css'
 const Form=()=>{
+    const [name,setName]=useState('');
+    const [email,setEmail]=useState('');
 
     const Submit=(e)=>{
-        e.preventDefault();    // if we want our to not refresh the page after submitting the form then add this
-        console.log("i am submitted");
+        e.preventDefault();    
+        console.log(name,email);
     }
     return (
-        <>``
+        <>
         <article>
-            <form class='form' onSubmit={Submit}>    {/*by adding onsubmit we can change the behavior while submitting form*/}
+            <form className='form' onSubmit={Submit}>    {/* to save the input value first we have to declare a state variables and initially assign them empty*/}
                 <div className="form-content">
                     <label htmlFor="formName">Name : </label>
-                    <input type="text" id="formName" name="formName"/>
+                    <input type="text" id="formName" name="formName" value={name}  //to add that variable with initial valuw with form add value attribute in the input tag this will put the initial value into the form
+                    onChange={(e)=>setName(e.target.value)}/>  {/*to save the input value we have to add attribute onchange into the input which will save the changed value inside it we will pass the event and that value state and inside state we will trigger e.target.value so whatever there is a change in the value will be saved in variable */}
                 </div>
                 <div className="form-content">
                     <label htmlFor="formEmail">Email : </label>
-                    <input type="text" id="formEmail" name="formEmail"/>
+                    <input type="text" id="formEmail" name="formEmail" value={email}
+                    onChange={(e)=>setEmail(e.target.value)}/>
                 </div>
                 <button type="submit">Submit</button>
-                {/* <button type="submit" onClick={Submit}>Submit</button>    instead of writing onsubmit on form we can also add it in onclick in button that whatwe want to do when we click on submit so both will do the same thing */}
 
             </form>
         </article>
