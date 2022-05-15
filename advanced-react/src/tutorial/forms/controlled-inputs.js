31e6abafb236088b8ea085da9c630aae9fcaffe1
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-15 14:24:28 +0530

    add saved items to the list

--- advanced-react/src/tutorial/forms/controlled-inputs.js
+++ advanced-react/src/tutorial/forms/controlled-inputs.js
@@ -11,11 +11,11 @@ const Form=()=>{
     return (
         <>
         <article>
-            <form className='form' onSubmit={Submit}>    {/* to save the input value first we have to declare a state variables and initially assign them empty*/}
+            <form className='form' onSubmit={Submit}>    
                 <div className="form-content">
                     <label htmlFor="formName">Name : </label>
-                    <input type="text" id="formName" name="formName" value={name}  //to add that variable with initial valuw with form add value attribute in the input tag this will put the initial value into the form
-                    onChange={(e)=>setName(e.target.value)}/>  {/*to save the input value we have to add attribute onchange into the input which will save the changed value inside it we will pass the event and that value state and inside state we will trigger e.target.value so whatever there is a change in the value will be saved in variable */}
+                    <input type="text" id="formName" name="formName" value={name}  
+                    onChange={(e)=>setName(e.target.value)}/> 
                 </div>
                 <div className="form-content">
                     <label htmlFor="formEmail">Email : </label>
