a726f1b9e83a62d2c243b698bfa5d2bc763174d9
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-16 15:20:53 +0530

    useref uses

--- advanced-react/src/tutorial/use-ref/useref-basics.js
+++ advanced-react/src/tutorial/use-ref/useref-basics.js
@@ -1,9 +1,17 @@
-import React from "react";
-
+import React ,{useEffect,useRef} from "react";
+import '../../App.css';
 const UseRefBasics=()=>{
+    const refContainer=useRef(null);   // useref variable is defined by this syntax
+    const Submit=(e)=>{
+        e.preventDefault();
+        console.log(refContainer.current.value)
+    }
     return(
         <>
-        hello
+        <form className="form" onSubmit={Submit}>
+            <input type="text" ref={refContainer}/>  {/*if we want to include the useref then we have to include the ref attribute in any tag to access it */}
+            <button type="submit">Submit</button>
+        </form>
         </>
     );
 }
