5f9535d5cb56c57895e3f8ab1e37f14f6b58eeef
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-16 15:24:06 +0530

    accessing containers using useref

--- advanced-react/src/tutorial/use-ref/useref-basics.js
+++ advanced-react/src/tutorial/use-ref/useref-basics.js
@@ -2,9 +2,11 @@ import React ,{useEffect,useRef} from "react";
 import '../../App.css';
 const UseRefBasics=()=>{
     const refContainer=useRef(null);   // useref variable is defined by this syntax
+    const hContainer=useRef(null);
     const Submit=(e)=>{
         e.preventDefault();
-        console.log(refContainer.current.value)
+        console.log(refContainer.current.value)  // by this we can access the containers value
+        console.log(hContainer.current)  // by this we can access the whole container
     }
     return(
         <>
@@ -12,6 +14,9 @@ const UseRefBasics=()=>{
             <input type="text" ref={refContainer}/>  {/*if we want to include the useref then we have to include the ref attribute in any tag to access it */}
             <button type="submit">Submit</button>
         </form>
+        <h1 ref={hContainer}>
+            i am a form
+        </h1>
         </>
     );
 }
