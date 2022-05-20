f1866b2b3957c9927815f91fa4195dcc10193e0c
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-21 00:17:24 +0530

    remove users using prop drilling

--- advanced-react/src/tutorial/prop-drilling/prop-drilling.js
+++ advanced-react/src/tutorial/prop-drilling/prop-drilling.js
@@ -1,9 +1,36 @@
-import React from "react";
-
+import React,{useState} from "react";
+import {data} from '../../data';
 const PropDrilling=()=>{
+    const [people,setPeople]=useState(data);
+    const Removeperson=(id)=>{
+        setPeople((people)=>{
+            return people.filter((person)=>person.id!=id);
+        });
+    }
     return(
         <>
         <h1>prop drilling</h1>
+        <List people={people} Removeperson={Removeperson}/>
+        </>
+    );
+}
+
+const List=({people,Removeperson})=>{
+    return(
+        <>
+        {people.map((person)=>{
+            return <Singleperson key={person.id} {...person} Removeperson={Removeperson}/>;
+        })}
+        </>
+    );
+}
+const Singleperson=({id,name,Removeperson})=>{
+    return(
+        <>
+        <h4 className="user">
+            {name}
+            <button className="btn" onClick={()=>Removeperson(id)}>Remove</button>
+        </h4>
         </>
     );
 }
