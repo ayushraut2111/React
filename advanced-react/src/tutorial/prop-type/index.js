00d5906fc2d67ccf49d8579f4d2538240422045d
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-22 00:45:39 +0530

    prop type throw error

--- advanced-react/src/tutorial/prop-type/index.js
+++ advanced-react/src/tutorial/prop-type/index.js
@@ -3,18 +3,27 @@ import {useFetch} from '../custom-hooks/useFetch'
 import { Product } from "./products";
 const url='https://api.github.com/users'
 const Index=()=>{
-    const {products}=useFetch(url);
-    console.log(products)
+    // const {products}=useFetch(url);
+    // console.log(products)
+    // return(
+    //     <>
+    //     <h1>
+    //         -----Products-----
+    //     </h1>
+    //     <div>
+    //         {products.map((product)=>{
+    //             return <Product key={product.id} {...product}/>
+    //         })}
+    //     </div>
+    //     </>
+    const abc={id:1,name:'ayush',class:{k:1}};
     return(
         <>
-        <h1>
-            -----Products-----
-        </h1>
-        <div>
-            {products.map((product)=>{
-                return <Product key={product.id} {...product}/>
-            })}
-        </div>
+        <List key={abc.id}abc={abc}/>
         </>
     );}
+
+const List=({abc})=>{
+    return(<h1>{abc.class.j}</h1>)  // this will throw an error
+}
 export default Index;
