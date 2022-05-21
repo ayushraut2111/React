d1a4d96514ebae66e6abe1bd6586e92e3f95f63c
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-22 00:46:43 +0530

    prop types

--- advanced-react/src/tutorial/prop-type/index.js
+++ advanced-react/src/tutorial/prop-type/index.js
@@ -3,27 +3,18 @@ import {useFetch} from '../custom-hooks/useFetch'
 import { Product } from "./products";
 const url='https://api.github.com/users'
 const Index=()=>{
-    // const {products}=useFetch(url);
-    // console.log(products)
-    // return(
-    //     <>
-    //     <h1>
-    //         -----Products-----
-    //     </h1>
-    //     <div>
-    //         {products.map((product)=>{
-    //             return <Product key={product.id} {...product}/>
-    //         })}
-    //     </div>
-    //     </>
-    const abc={id:1,name:'ayush',class:{k:1}};
+    const {products}=useFetch(url);
+    console.log(products)
     return(
         <>
-        <List key={abc.id}abc={abc}/>
+        <h1>
+            -----Products-----
+        </h1>
+        <div>
+            {products.map((product)=>{
+                return <Product key={product.id} {...product}/>
+            })}
+        </div>
         </>
-    );}
-
-const List=({abc})=>{
-    return(<h1>{abc.class.j}</h1>)  // this will throw an error
-}
+    )}
 export default Index;
