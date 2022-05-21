b8c6f1d60f6fb9213107804bd17e1543fee8cc2a
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-21 23:06:24 +0530

    making our own custom hooks

--- advanced-react/src/tutorial/custom-hooks/fetch-example.js
+++ advanced-react/src/tutorial/custom-hooks/fetch-example.js
@@ -1,11 +1,12 @@
-import React from "react";
-
-const Example=()=>{
+import React,{useState,useEffect} from "react";
+import {useFetch} from './useFetch'
+const url='https://api.github.com/users'
+const Example=()=>{        // we can make our custom hooks so that we can reuse it for any component also, so to make it declare it with use... keyword
+    // and putting it in another js file so that we can import it and pass the information which it needed and then take the returned object and use it in main file
+    const {products,loading}=useFetch(url);
     return(
         <>
-        <h1>
-            hello
-        </h1>
+        {loading?<h3>loading....</h3>:<h3>Data</h3>}
         </>
     );
 }
